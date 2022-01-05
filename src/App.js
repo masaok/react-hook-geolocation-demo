dc2f02e0b84155960c2fd52504c70dec9d690293
Masao Kitamura <masaok@gmail.com>
2022-01-05 06:18:33 -0800

    Fixing text align to left

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          <li>Latitude: {geolocation.latitude}</li>
-          <li>Longitude: {geolocation.longitude}</li>
-          <li>Location accuracy: {geolocation.accuracy}</li>
-          <li>Altitude: {geolocation.altitude}</li>
-          <li>Altitude accuracy: {geolocation.altitudeAccuracy}</li>
-          <li>Heading: {geolocation.heading}</li>
-          <li>Speed: {geolocation.speed}</li>
-          <li>Timestamp: {geolocation.timestamp}</li>
-        </ul>
+        <section className="content">
+          <ul>
+            <li>Latitude: {geolocation.latitude}</li>
+            <li>Longitude: {geolocation.longitude}</li>
+            <li>Location accuracy: {geolocation.accuracy}</li>
+            <li>Altitude: {geolocation.altitude}</li>
+            <li>Altitude accuracy: {geolocation.altitudeAccuracy}</li>
+            <li>Heading: {geolocation.heading}</li>
+            <li>Speed: {geolocation.speed}</li>
+            <li>Timestamp: {geolocation.timestamp}</li>
+          </ul>
+        </section>
       </header>
     </div>
   )
